67f52678c24b99b30b442a99cdd53112b0dacda5
Sam <samuel.waters@gmail.com>
2023-03-04 23:22:10 -0700

    chore:added calendar code and import

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,7 +1,10 @@
+import CalendarComp from "../../components/Calender"
+
 export default function Calendar() {
     return (
         <>
             <h1>Calendar page</h1>
+            <CalendarComp />
         </>
     )
 }
