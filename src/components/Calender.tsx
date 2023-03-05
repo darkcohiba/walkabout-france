0e2ee034142f111b7ee711c92f37b0a5e29bb7d3
Sam <samuel.waters@gmail.com>
2023-03-04 23:52:30 -0700

    feat:added fetch hook and trying to implement

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -17,50 +17,7 @@ import {
 } from 'date-fns'
 import { Fragment, useState } from 'react'
 import Meeting from "./Meeting"
-
-const meetings = [
-  {
-    id: 1,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2023-03-11T13:00',
-    endDatetime: '2023-03-11T14:30',
-  },
-  {
-    id: 2,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2023-03-20T09:00',
-    endDatetime: '2023-03-20T11:30',
-  },
-  {
-    id: 3,
-    name: 'Dries Vincent',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2023-03-20T17:00',
-    endDatetime: '2023-03-20T18:30',
-  },
-  {
-    id: 4,
-    name: 'Leslie Alexander',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2023-03-09T13:00',
-    endDatetime: '2023-03-09T14:30',
-  },
-  {
-    id: 5,
-    name: 'Michael Foster',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    startDatetime: '2023-03-13T14:00',
-    endDatetime: '2023-03-13T14:30',
-  },
-]
-
+import useFetch from '@/hooks/useFetchHook'
 
 
 export default function Example() {
@@ -68,6 +25,10 @@ export default function Example() {
   let [selectedDay, setSelectedDay] = useState(today)
   let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
   let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
+
+  const [url, setUrl] = useState("http://localhost:3000/meetings")
+
+  const { isLoading, serverError, apiData } = useFetch(url);
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -88,7 +49,7 @@ export default function Example() {
     setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
 
-  let selectedDayMeetings = meetings.filter((meeting) =>
+  let selectedDayMeetings = apiData.filter((meeting) =>
     isSameDay(parseISO(meeting.startDatetime), selectedDay)
   )
 
@@ -168,7 +129,7 @@ export default function Example() {
                   </button>
 
                   <div className="w-1 h-1 mx-auto mt-1">
-                    {meetings.some((meeting) =>
+                    {apiData.some((meeting) =>
                       isSameDay(parseISO(meeting.startDatetime), day)
                     ) && (
                       <div className="w-1 h-1 rounded-full bg-sky-500"></div>
---
+++ src/hooks/useFetchHook.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+
+const useFetch = (url) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [apiData, setApiData] = useState(null);
+    const [serverError, setServerError] = useState(null);
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchData = async () => {
+            try {
+                const resp = await fetch(url);
+                const data = await resp.json();
+                setApiData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setServerError(error);
+                setIsLoading(false);
+            }
+        };
+        fetchData();
+    }, [url]);
+    return { isLoading, apiData, serverError };
+}
+
+export default useFetch;
