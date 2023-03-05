bc458e717361ce7c3f7e11ba5e0c553284d5a01b
Sam <samuel.waters@gmail.com>
2023-03-05 00:21:00 -0700

    chore: added rendering of name on each calender vent

--- src/pages/calendar/[pid].tsx
+++ src/pages/calendar/[pid].tsx
@@ -1,10 +1,21 @@
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 const Post = () => {
+    const [meeting, setMeeting] = useState([])
     const router = useRouter()
     const { pid } = router.query
+    useEffect(()=>{
+        fetch(`http://localhost:3000/meetings/${pid}`)
+        .then(response => response.json())
+        .then(data => setMeeting(data))
+    },[])
+    console.log(meeting)
 
-    return <p>Post: {pid}</p>
+    return(
+        <>
+        </>
+    )
 }
 
 export default Post
