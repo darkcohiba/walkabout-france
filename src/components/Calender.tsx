bc05599730224b46fbc94c4373d61bbd371625ce
Sam <samuel.waters@gmail.com>
2023-03-05 00:00:34 -0700

    chore: back tracked

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -17,7 +17,50 @@ import {
 } from 'date-fns'
 import { Fragment, useState } from 'react'
 import Meeting from "./Meeting"
-import useFetch from '@/hooks/useFetchHook'
+
+const meetings = [
+  {
+    id: 1,
+    name: 'Leslie Alexander',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2023-03-11T13:00',
+    endDatetime: '2023-03-11T14:30',
+  },
+  {
+    id: 2,
+    name: 'Michael Foster',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2023-03-20T09:00',
+    endDatetime: '2023-03-20T11:30',
+  },
+  {
+    id: 3,
+    name: 'Dries Vincent',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2023-03-20T17:00',
+    endDatetime: '2023-03-20T18:30',
+  },
+  {
+    id: 4,
+    name: 'Leslie Alexander',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2023-03-09T13:00',
+    endDatetime: '2023-03-09T14:30',
+  },
+  {
+    id: 5,
+    name: 'Michael Foster',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2023-03-13T14:00',
+    endDatetime: '2023-03-13T14:30',
+  },
+]
+
 
 
 export default function Example() {
@@ -25,10 +68,6 @@ export default function Example() {
   let [selectedDay, setSelectedDay] = useState(today)
   let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
   let firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date())
-
-  const [url, setUrl] = useState("http://localhost:3000/meetings")
-
-  const { isLoading, serverError, apiData } = useFetch(url);
 
   function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -49,7 +88,7 @@ export default function Example() {
     setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'))
   }
 
-  let selectedDayMeetings = apiData.filter((meeting) =>
+  let selectedDayMeetings = meetings.filter((meeting) =>
     isSameDay(parseISO(meeting.startDatetime), selectedDay)
   )
 
@@ -129,7 +168,7 @@ export default function Example() {
                   </button>
 
                   <div className="w-1 h-1 mx-auto mt-1">
-                    {apiData.some((meeting) =>
+                    {meetings.some((meeting) =>
                       isSameDay(parseISO(meeting.startDatetime), day)
                     ) && (
                       <div className="w-1 h-1 rounded-full bg-sky-500"></div>
