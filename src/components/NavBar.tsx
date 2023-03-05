6c45a3962ff9387f9c37ba620f8ce94809e5e190
Sam <samuel.waters@gmail.com>
2023-03-04 22:33:03 -0700

    chore:css links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link'
 
 
 export default function NavBar() {
-    const navItemStyle = "hover:bg-slate-600 rounded p-2"
+    const navItemStyle = "bg-earth h-10 hover:bg-green  hover:animate-pulse rounded p-2 m-auto"
     return(
         <>
-            <ul className='flex justify-between m-2'>
+            <ul className='bg-sand h-20 rounded-lg flex justify-evenly m-2'>
+                <img className='rounded-lg h-3/4 my-auto mx-2' src="https://kitsaphapps.com/wp-content/uploads/2022/04/open-uri20150313-11-vr3cz.png"/>
                 <li className={navItemStyle}>
                     <Link href="/">Home</Link>
                 </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hike-water': "url('http://easyhiker.co.uk/wp-content/uploads/2012/09/95781fea_smush_7987531752_47ce9eeaff_z.jpg')",
+        'logo': "url('https://embed.tumblr.com/embed/post/t:J6Di_a2KtOmsEDddW-M7mw/710927855049113600/v2')"
+      },
+      colors: {
+        'brown': '#52360d',
+        'earth':'#6b9b82',
+        'green':'#8ec4a8',
+        'sand':'#e5dc7b',
+        'orange':'#e9a33a',
       },
     }
   },
