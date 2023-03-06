15d23b52b03c455f50eee029602d675e6715079e
Sam <samuel.waters@gmail.com>
2023-03-05 18:56:33 -0700

    chore: works but it gives me  warning that it mgiht be undefined which is nice

--- src/pages/calendar/[pid].tsx
+++ src/pages/calendar/[pid].tsx
@@ -3,16 +3,26 @@ import { useEffect, useState } from 'react'
 import { Hike } from '../../types'
 
 const Post = () => {
-    const [meeting, setMeeting] = useState<Hike[]>([])
+    const [meeting, setMeeting] = useState<Hike>()
+    const [loaded, setLoaded] = useState(false)
 
     const router = useRouter()
     const { pid } = router.query
     useEffect(()=>{
         fetch(`http://localhost:3000/meetings/${pid}`)
         .then(response => response.json())
-        .then(data => setMeeting(data))
+        .then(data => {
+            setMeeting(data)
+            setLoaded(true)
+        })
+        .catch(error => {
+            console.log(error)
+            setLoaded(false)
+        })
     },[pid])
     console.log(meeting)
+
+    if (!loaded) return <p>Loading...</p>
 
     return(
         <>
